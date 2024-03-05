b1de62ddbc351f9e2a39123327146955855fd7cd
Vadim Medvedev <vadim130809@gmail.com>
2024-03-05 21:27:52 +0300

    add logout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,35 @@
-import {FC} from 'react'
+import {FC, useContext} from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from '../UI/Button'
 import { Sizes } from '../utils/enums'
+import { AuthContext } from '../context'
 
 export const Navbar:FC = () => {
+  const {setIsAuth} = useContext(AuthContext)
+  
   return (
-    <div className='bg-slate-700 sticky top-0 z-10'>
-      <ul className='flex gap-2 p-2'>
-        <li>
-          <Link to='/posts'>
-            <Button size={Sizes.large}>
-              Посты
-            </Button>
-          </Link>
-        </li>
-        <li>
-          <Link to='/about'>
-            <Button size={Sizes.large}>
-              О нас
-            </Button>
-          </Link>
-        </li>
-      </ul>
-    </div>
+    <header className='bg-slate-700 sticky top-0 z-10 flex items-center 
+      justify-between px-2'
+    >
+      <nav>
+        <ul className='flex gap-2 p-2'>
+          <li>
+            <Link to='/posts'>
+              <Button size={Sizes.large}>
+                Посты
+              </Button>
+            </Link>
+          </li>
+          <li>
+            <Link to='/about'>
+              <Button size={Sizes.large}>
+                О нас
+              </Button>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Button size={Sizes.large} onClick={():void => setIsAuth(false)}>Выйти</Button>
+    </header>
   )
 }
